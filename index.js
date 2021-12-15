b42f6635f7c4a0584419a5b24afc91f1637b369b
Maria Lago <50707572+marialago@users.noreply.github.com>
2021-12-15 14:08:59 -0300

    up

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const server = express();
-//configurar pasta publica
 server.use(express.static("public"));
-//utilizando template engine
 const nunjucks = require("nunjucks");
 nunjucks.configure("src/views", {
   express: server,
   noCache: true,
 });
-//configurar caminhos da aplicação/ pag inicial
-// req: requisição,pedido / res: resposta
 server.get("/", (req, res) => {
   return res.render("index.html", {title: "Um titulo"});
 });
 server.get("/create-point", (req, res) => {
   return res.render("cadastro.html");
 });
-//ligar servidor
 server.listen(3000);
